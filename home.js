65bb0d2978f80cd7f597801f08b6468e2746475d
sophea2105 <138038728+sophea2105@users.noreply.github.com>
2023-08-22 16:30:29 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,50 @@
+document.getElementById("phone").style.display="none";
+function phone(){
+    document.getElementById("product").style.display="none";
+    document.getElementById("phone").style.display="inline";
+    document.getElementById("slide_img2").src="https://www.samsung.com/global/galaxy/main/images/kv-galaxy-s23_s.jpg";
+    document.getElementById("slide_img3").src="https://i.brecorder.com/primary/2022/06/62a552eed243d.jpg";
+    document.getElementById("slide_img4").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2SwQCVx7jhMmIxhTuMGiXoez5g2xPYRDtkA&usqp=CAU";
+    document.getElementById("slide_img5").src="https://cdn.vox-cdn.com/thumbor/WGfqNpljNczLXLhVCekh-37BWZA=/0x0:2100x1400/1400x1400/filters:focal(1342x704:1343x705)/cdn.vox-cdn.com/uploads/chorus_asset/file/24464849/bg_88fb5fbaca.jpg";
+    document.getElementById("slide_img6").src="https://mytop.techinfus.com/wp-content/uploads/2020/04/huawei-p40-pro-rear-cover-full_large.jpg";
+    }
+function iphone(){
+    document.getElementById("product").style.display="none";
+    document.getElementById("oppo").style.display="none";
+    document.getElementById("vivo").style.display="none";
+    document.getElementById("samsung").style.display="none";
+    document.getElementById("phone").style.display="inline";
+    document.getElementById("iphone").style.display="inline";
+    document.getElementById("drop_menu").style.display="none";
+
+}
+function oppo(){
+    document.getElementById("product").style.display="none";
+    document.getElementById("oppo").style.display="none";
+    document.getElementById("iphone").style.display="none";
+    document.getElementById("samsung").style.display="none";
+    document.getElementById("phone").style.display="inline";
+    document.getElementById("oppo").style.display="inline";
+    document.getElementById("drop_menu").style.display="none";
+
+}
+function samsung(){
+    document.getElementById("product").style.display="none";
+    document.getElementById("oppo").style.display="none";
+    document.getElementById("vivo").style.display="none";
+    document.getElementById("iphone").style.display="none";
+    document.getElementById("phone").style.display="inline";
+    document.getElementById("samsung").style.display="inline";
+    document.getElementById("drop_menu").style.display="none";
+
+}
+function vivo(){
+    document.getElementById("product").style.display="none";
+    document.getElementById("oppo").style.display="none";
+    document.getElementById("iphone").style.display="none";
+    document.getElementById("samsung").style.display="none";
+    document.getElementById("drop_menu").style.display="none";
+    document.getElementById("phone").style.display="inline";
+    document.getElementById("vivo").style.display="inline";
+
+}
